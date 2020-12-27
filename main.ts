efa31e63a297caa998b4f9b34607b9aa302fa4fc
richdartingfruit <76633464+richdartingfruit@users.noreply.github.com>
2020-12-27 11:50:14 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onPinReleased(TouchPin.P2, function () {
+    basic.showString("Cool")
+})
+input.onPinReleased(TouchPin.P0, function () {
+    basic.showString("Hi")
+})
 input.onPinReleased(TouchPin.P1, function () {
     basic.showString("Hello!")
 })
